1a46cd279146fcde7b6263de767862ae499612d2
Code <+Stevan80@users.noreply.github.com>
2024-04-05 20:42:21 +0200

    Any changes

---
+++ DataAccess/Storage.cs
@@ -0,0 +1,12 @@
+﻿using Models;
+using System.Data;
+
+namespace DataAccess
+{
+    //Database with multiple storage sets that represent Tables/Collections per type
+    public static class Storage
+    {
+        public static StorageSet<User> Users { get; set; }
+        public static StorageSet<Car> Cars { get; set; }
+    }
+}
